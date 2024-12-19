cfa56360c5b16717c877003be748427809f8e2e9
yasahiro10 <yassineouardani381@gmail.com>
2024-12-19 10:30:02 +0100

    GradioBlocks

--- gradio app/tut_5/tut_5.py
+++ gradio app/tut_5/tut_5.py
@@ -9,4 +9,5 @@ if __name__ == '__main__':
         output = gr.Textbox(label='Output')
         gr_button = gr.Button('Cool!')
         gr_button.click(fn=return_hello_with_name,inputs=name,outputs=output,api_name='block_try')
+
     demo.launch()
